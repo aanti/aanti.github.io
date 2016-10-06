8211b6e0662d65e58660de38ca47274e09b18550
aantii@o2.pl <verlore4>
2016-10-06 13:55:54 +0200

    test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Brunch + Redux + React + Babel/ES6 Skeleton with my personal favourite settings
+
+This is a modern JS skeleton with React and Redux for [Brunch](http://brunch.io).
+
---
+++ app/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.app = null
+  }
+  
+  render () {
+    return (
+      <div id="content">
+        {/* main content */}
+        asdasdasdasdasda
+      </div>
+    )
+  }
+}
